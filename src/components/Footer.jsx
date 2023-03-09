7a56c805359635d679df1d81b7d64f9436b171f0
dariobartoli <113705473+dariobartoli@users.noreply.github.com>
2023-03-08 21:06:55 -0300

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ export const Footer = () => {
   return (
     <>
     <div className='footer'>
-        <a href="https://github.com/dariobartoli" target="_blank"><img src="./public/assets/image/github.png" alt="github" className='image__footer'/></a>
-        <a href="https://www.linkedin.com/in/dar%C3%ADo-bartoli-8b5848259/" target="_blank"><img src="./public/assets/image/linkedin.png" alt="linkedin" className='image__footer'/></a>
+        <a href="https://github.com/dariobartoli" target="_blank"><img src="./assets/image/github.png" alt="github" className='image__footer'/></a>
+        <a href="https://www.linkedin.com/in/dar%C3%ADo-bartoli-8b5848259/" target="_blank"><img src="./assets/image/linkedin.png" alt="linkedin" className='image__footer'/></a>
     </div>
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,10 @@ export const Header = () => {
         <div className='header'>
             <h1 className='animate__animated animate__zoomIn'>¿A que casa de Hogwarts perteneces?</h1>
             <div className='image__container'>
-                <img src="./public/assets/image/gryffindor.png" alt="logocasa" className='image__header'/>
-                <img src="./public/assets/image/hufflepuff.png" alt="logocasa" className='image__header'/>
-                <img src="./public/assets/image/slytherin.png" alt="logocasa" className='image__header'/>
-                <img src="./public/assets/image/ravenclaw.png" alt="logocasa" className='image__header'/>
+                <img src="./assets/image/gryffindor.png" alt="logocasa" className='image__header'/>
+                <img src="./assets/image/hufflepuff.png" alt="logocasa" className='image__header'/>
+                <img src="./assets/image/slytherin.png" alt="logocasa" className='image__header'/>
+                <img src="./assets/image/ravenclaw.png" alt="logocasa" className='image__header'/>
             </div>
         </div>
     </>
